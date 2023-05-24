d5256f99340751d3dcc0ae407a267c867e0f224e
admitn <admitn91@gmail.com>
2023-05-24 22:17:26 +0300

    admitn
    load for media tech

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/oc_test/admin/');
+define('HTTP_CATALOG', 'http://localhost/oc_test/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/oc_test/admin/');
+define('HTTPS_CATALOG', 'http://localhost/oc_test/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/OpenServer/domains/localhost/oc_test/admin/');
+define('DIR_SYSTEM', 'D:/OpenServer/domains/localhost/oc_test/system/');
+define('DIR_IMAGE', 'D:/OpenServer/domains/localhost/oc_test/image/');
+define('DIR_LANGUAGE', 'D:/OpenServer/domains/localhost/oc_test/admin/language/');
+define('DIR_TEMPLATE', 'D:/OpenServer/domains/localhost/oc_test/admin/view/template/');
+define('DIR_CONFIG', 'D:/OpenServer/domains/localhost/oc_test/system/config/');
+define('DIR_CACHE', 'D:/OpenServer/domains/localhost/oc_test/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/OpenServer/domains/localhost/oc_test/system/storage/download/');
+define('DIR_LOGS', 'D:/OpenServer/domains/localhost/oc_test/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/OpenServer/domains/localhost/oc_test/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/OpenServer/domains/localhost/oc_test/system/storage/upload/');
+define('DIR_CATALOG', 'D:/OpenServer/domains/localhost/oc_test/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'oc_store-test');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
